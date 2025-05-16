7c67ebe01ff98da0adebbba630bd04ff719b7af1
LILLOLLAL <Gillbrian079@gmail.com>
2025-05-16 21:40:03 +0700

    Nambahin utils baru dan memperbarui Main

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
     utils.SayHello("Alice")
+	utils.search()
 }
 
 //MAKAN BANG
---
+++ utils/searchAlgo.go
@@ -0,0 +1,7 @@
+package utils
+
+import "fmt"
+
+func search(){
+	fmt.Println("Coba Lagi")
+}
